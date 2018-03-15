cd77b63244525327c016e5b7f1cb9a467fdc1239
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2018-03-15 11:53:11 -0500

    add functionality to get and populate items for the food list in the DOM

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ const ItemCtrl = (function(){
    }
    // public methods
    return {
+      getItems: function () {
+         return data.items;
+      },
       logData: function(){
          return data;
       }
@@ -31,10 +34,29 @@ const ItemCtrl = (function(){
 // UI Controller
 // UICtrl is set equal to IIFE that will automatically run
 const UICtrl = (function(){
-   
+   const UISelectors = {
+      itemList: '#item-list'
+   }
    // public method
    return {
+      populateItemList: function(items){
+         // loop through the items using forEach()
+         // Each item is made into a list-item and inserted in the DOM as a li. 
+         // Every time the loop runs, an li is added to the html variable.
+         let html = '';  
 
+         items.forEach(function(item){
+            html += `<li id="item-${item.id}" class="collection-item">
+               <strong>${item.name}: </strong> <em> ${item.calories} calories</em>
+                  <a href="#" class="Secondary-content">
+                     <i class="edit-item fa fa-pencil"></i>
+                  </a>
+               </li>`;
+         });
+         // Inserts list-items
+         // appends the list items to the foods list
+         document.querySelector(UISelectors.itemLlist).innerHTML = html;
+      }
    }
 })();
 
@@ -55,7 +77,19 @@ const App = (function(ItemCtrl, UICtrl){
 
    return {
       init: function (){
-         console.log('Initializing App...'); 
+         // Fetch items from data structur
+         const items = ItemCtrl.getItems();
+         console.log(items);
+         /* logged to the console:
+            (3) [{…}, {…}, {…}]
+               0:{id: 0, name: "Steak dinner", calories: 1200}
+               1:{id: 1, name: "cookie", calories: 300}
+               2:{id: 2, name: "wine", calories: 200}
+               length: 3
+         */
+
+         // Populate ul with the items; pass in items that were fetched with the getItems()
+         UICtrl.populateItemList(items);
       }
    }
 })(ItemCtrl, UICtrl);
